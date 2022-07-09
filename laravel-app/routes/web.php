77d6bb210c3347ad0c45ce8bceef7e87eb6cae0d
newyee <takatelin01@icloud.com>
2022-07-09 11:54:37 +0900

    ログイン画面 | ユーザー登録画面の作成

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('login',[LoginController::class, 'index'])->name('login.index');
+Route::get('register',[RegisterController::class, 'index'])->name('register.index');
